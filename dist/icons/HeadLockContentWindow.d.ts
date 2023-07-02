e46b3f887a014aa198dfa95b8ce0844912c8aa80
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-07-02 15:30:12 +0300

    chore: build

---
+++ dist/icons/HeadLockContentWindow.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+declare const Memo: React.MemoExoticComponent<({ width, height, color, strokeWidth, ...props }: SVGProps<SVGSVGElement>) => JSX.Element>;
+export default Memo;
